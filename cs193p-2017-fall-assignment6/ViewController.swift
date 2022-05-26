22850f0b4e0036288900e83e6c54abf6630c91fb
ksurikova <ksantisurikova@gmail.com>
2022-05-26 14:32:14 +0300

    Initial Commit

---
+++ cs193p-2017-fall-assignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cs193p-2017-fall-assignment6
+//
+//  Created by Ksenia Surikova on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
